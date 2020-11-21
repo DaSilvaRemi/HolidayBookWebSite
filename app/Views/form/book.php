bb70875ca402d70c41908e96d941456c88c9d5a5
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2020-11-21 17:24:01 +0100

    Ajouts de la selection d'option

--- app/Views/form/book.php
+++ app/Views/form/book.php
@@ -4,32 +4,34 @@
 </head>
 <body>
 
-    <?= $validation->listErrors() ?>
-    <?= form_open('form') ?>
+    <?= $validation->listErrors(); ?>
+    <?= form_open('BookForm'); ?>
 
     <h5>Type Logement</h5>
     <select name="typelogement">
         <option value="">-----Veuillez sélectionnez une option------</option>
         <?php 
-        $tabTypeLogemet = \App\Controllers\BookForm::showData();
-        foreach ($tabTypeLogemet as $typelogement) {
-            echo '<option value="'.$typelogement['typelogement'].'">'.$typelogement['typelogement'].'</option>';
- } ?>    
+        $tabQuery = \App\Controllers\BookForm::showData();
+        foreach ($tabQuery as $tabTypeLogement) {
+            var_dump($tabTypeLogement);
+                echo '<option value="'.$tabTypeLogement["typelogement"].'">'.$tabTypeLogement["typelogement"].'</option>';  
+        } 
+ ?> 
+           
     </select>
-
     
     <h5>Date debut</h5>
     <input type="date" name="datedebut" min="2020-11-07" size="50" step="7"/>
 
     <h5>Date de Fin</h5>
-    <input type="date" name="datefin" min="2020-11-07" size="50" step="7" />
+    <input type="date" name="datefin" min="2020-11-07" size="50" step="7"/>
 
     <h5>Nombre de personne</h5>
     <input type="number" name="nbpersonne" min="1" max="4" value="1" size="50" />
 
     <h5>Choix de la pension</h5>
     <select name="pension">
-        <option value=""></option>
+        <option value="">-----Veuillez sélectionnez une option-----</option>
         <option value="pensioncomplete">pension complète</option>
         <option value="demipension">demi-pension</option>
     </select>
