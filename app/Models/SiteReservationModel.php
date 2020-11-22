6bd3a9df934f99da5ec14fd41061d7221ef0b1a2
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2020-11-22 15:08:35 +0100

    Ajout des méthodes pour la table user

--- app/Models/SiteReservationModel.php
+++ app/Models/SiteReservationModel.php
@@ -23,21 +23,21 @@ class SiteReservationModel extends Model{
     retour : retourne un tableau contenant les résultat de la requête     */
     public function getTypeLogement($typelogement = ""){
         if(empty($typelogement)){
-            return  $this->db->query("SELECT typelogement FROM typelogement;")->getResultArray();
+            return  $this->db->query("SELECT typelogement FROM public.typelogement;")->getResultArray();
         }
         else{
-            return $this->db->query("SELECT typelogement FROM typelogement WHERE typelogement = :typelogement: LIMIT 1;",["typelogement" => $typelogement])->getResultArray();
+            return $this->db->query("SELECT typelogement FROM public.typelogement WHERE typelogement = :typelogement: LIMIT 1;",["typelogement" => $typelogement])->getResultArray();
         }
     }
 
     /*Retourne la requete pour le nombre de lit double*/
     public function getNbLitDouble($typelogement){
-        return $this->db->query("SELECT nblitdouble FROM typelogement WHERE typelogement = :typelogement: ORDER BY typelogement DESC;",["typelogement" => $typelogement])->getResultArray();
+        return $this->db->query("SELECT nblitdouble FROM public.typelogement WHERE typelogement = :typelogement: ORDER BY typelogement DESC;",["typelogement" => $typelogement])->getResultArray();
     }
 
     /*Retourne la requete pour le nombre de lit simple*/
     public function getNbLitSimple($typelogement){
-        return  $this->db->query("SELECT nblitsimple FROM typelogement WHERE typelogement = :typelogement: ORDER BY typelogement DESC;",["typelogement" => $typelogement])->getResultArray();
+        return  $this->db->query("SELECT nblitsimple FROM public.typelogement WHERE typelogement = :typelogement: ORDER BY typelogement DESC;",["typelogement" => $typelogement])->getResultArray();
     }
     
     /*--------------------------------------Table réservation------------------------------------------------*/
@@ -47,7 +47,7 @@ class SiteReservationModel extends Model{
      * valide => boolean => setByDefault(true) => Champs de validation
     -retour : void     */
     public function alterIsValide($idSejour, $valide = true) : void{
-        $this->db->query("UPDATE reservation SET valide = :valide: WHERE id_sejour = :id_sejour:;",["valide" => $valide, "is_sejour" => $idSejour]);
+        $this->db->query("UPDATE public.reservation SET valide = :valide: WHERE id_sejour = :id_sejour:;",["valide" => $valide, "is_sejour" => $idSejour]);
     }
     
     /*
@@ -61,11 +61,25 @@ class SiteReservationModel extends Model{
      * menage => boolean => Correspond à option   
     -retour : void     */
     public function insertReservation($typelogement, $id_user,$dateDebut, $dateFin, $nbPersonne, $pension, $menage) : void{
-        $this->db->query("INSERT INTO reservation (datedebut, datefin, nbpersonne, pension, menage, valide, id_user, num_logement, typelogement) "
+        $this->db->query("INSERT INTO public.reservation (datedebut, datefin, nbpersonne, pension, menage, valide, id_user, num_logement, typelogement) "
                 . "VALUES(:datedebut:, :datefin:, :nbpersonne:, :pension:, :menage: , :valide:, :id_user:, "
                 . "(SELECT num_logement FROM logement WHERE typelogement = :typelogement:), :typelogement:);",
                 ["datedebut" => $dateDebut, "datefin" => $dateFin, "nbpersonne" => $nbPersonne, "pension" => $pension, "menage" => $menage, "valide" => false,
                     "id_user" => $id_user, "typelogement" => $typelogement,]);
     }
+    
+    /*------------------------------------Table user---------------------------------------------------------*/
+    public function getIdUser($login) {
+        return $this->db->query("SELECT id_user FROM public.user WHERE login = :login: ",["login" => $login])->getResultArray();
+    }
+    
+    public function countUserLogin($login){
+        return $this->db->query("SELECT COUNT(login) FROM public.user WHERE login = :login:",['login' => $login])->getResultArray();
+    }
+    
+    public function countIdUserValide($login, $mdp){
+        return $this->db->query("SELECT COUNT(id_user) FROM public.user WHERE login = :login: AND mdp = :mdp:",['login' => $login, 'mdp' => $mdp])->getResultArray();
+    }
+    
     
 }
