a75633904c161b647172bbc98f99c51eab57ed97
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2020-11-28 20:12:03 +0100

    Ajouts et modification des vues

--- app/Views/form/pageadmin.php
+++ app/Views/form/pageadmin.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <form>
+        <?= form_open('CreateUser'); ?>
         <table>
         <?php
         foreach ($tabReservation as $LesReservations) {
---
+++ app/Views/form/pageuser.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Réservation de séjour</title>
+</head>
+<body>
+
+    <h3>Votre réservation à été transmise !</h3>
+
+    <p><?php echo anchor('BookForm', 'Essayer à nouveau!'); ?></p>
+
+</body>
+</html>
+
