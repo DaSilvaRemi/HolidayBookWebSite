f0fb3ee91b572f2a200eb358cc36174663272af6
m-dodille <m-dodille@hote-Dodille>
2020-12-02 11:43:56 +0100

    Header avec les ancres

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -8,25 +8,25 @@ and open the template in the editor.
 <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="Home">CVVEN</a>
+      <?php echo anchor('Home', 'CVVEN','class="navbar-brand"'); ?>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="BookForm">Réservation
+            <?php echo anchor('BookForm', 'Reservations','class="nav-link"'); ?>
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="PageUser">Mes Réservations</a>
+            <?php echo anchor('PageUser', 'Mes reservations','class="nav-link"'); ?>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="Connexion" >Connexion</a>
+            <?php echo anchor('Connexion', 'Connexion','class="nav-link"'); ?>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="Connexion/deconnexion" >Déconnexion</a>
+            <?php echo anchor('Connexion/deconnexion', 'Deconnexion','class="nav-link"'); ?>
           </li>
         </ul>
       </div>
