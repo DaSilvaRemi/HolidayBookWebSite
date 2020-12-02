7a4c448f08b3712b3d2c31b15b9866cfa57511fa
m-dodille <m-dodille@hote-Dodille>
2020-12-02 10:25:23 +0100

    Le welcome message

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">CVVEN</a>
+      <a class="navbar-brand" href="Home">CVVEN</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -13,7 +13,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" target="_blank">Mes Réservations</a>
+            <a class="nav-link" href="PageUser">Mes Réservations</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="Connexion" >Connexion</a>
