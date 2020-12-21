26ee21339f1111722c5b2efcce77434be9ba8311
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2020-12-21 20:38:04 +0100

    Ajouts modification

--- app/Views/form/modifyreservation.php
+++ app/Views/form/modifyreservation.php
@@ -1,39 +1,52 @@
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html lang="fr">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>INSCRIPTION CVVEN</title>
+<title>Réservation de séjour</title>
 <link rel="stylesheet" href="<?= base_url('css/form.css'); ?>"> 
-
 </head>
+
 <body>
-<div class="register-form">
-    <?= form_open('CreateUser'); ?>
-        <h2 class="text-center">Modification administrateur</h2>       
+<div class="Sejour-form">
+    <?= form_open('ModifyReservation'); ?>
+        <h2 class="text-center">Effectuer réservation</h2>       
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Nom" id="nom" name="nom" value="<?php $infoUtilisateur['nom']?>" required="required">
-        </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Prenom" id="prenom" name="prenom" value="<?php $infoUtilisateur['prenom']?>" required="required">
-        </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Pseudo" id="user" name="user" value="<?php $infoUtilisateur['login']?>" required="required">
+            <select name="typelogement" class="form-control">
+                <option value="">-----Veuillez sélectionnez une option------</option>
+                    <?php 
+                        if(isset($tabTypeLogement)){
+                            echo '<option value="'.$tabTypeLogement["typelogement"].'">'.$tabTypeLogement["typelogement"].'</option>';  
+                        }   ?>        
+            </select>
+        </div>  
+        <div class="form-group">
+            <input type="date" name="datedebut" min="2021-01-02" size="50" step="7" value="<?php $tabInfoReservation['datedebut']; ?>" class="form-control"/>
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Mot de passe" id="password" name="password">
-            <input type="hidden" name="password2" value="<?php $infoUtilisateur['mdp']?>" />
+            <input type="date" name="datefin" min="2021-01-09" size="50" step="7" value="<?php $tabInfoReservation['datefin']; ?>" class="form-control"/>
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-block" id="Envoyer" >Modifier</button>
-            <br>
-            <?php echo anchor('Connexion', '<input class="btn btn-danger btn-block" value="Annuler" />'); ?>
+            <input type="number" name="nbpersonne" min="1" max="4" size="50"  value="<?php $tabInfoReservation['nbpersonne']; ?>" class="form-control"/>
         </div>
-        <div class="clearfix">
+        <div class="form-group">
+            <select name="pension" class="form-control">
+                <option value="">-----Veuillez sélectionnez une pension-----</option>
+                <option value="pensioncomplete">pension complète</option>
+                <option value="demipension">demi-pension</option>
+            </select>
+        </div>
+        <div class="form-check">
+            <label for="menage" class="form-check-label">
+            <input type="checkbox" name="menage" value="menage" class="form-check-input">
+            Ménage fin de séjour</label>
+        </div>
+   
+        <div class="form-group">
+            <?php if(isset($validation)){
+                echo $validation->listErrors();
+            }?>
+        </div>
+        <div class="form-group">
+            <input type="hidden" name="idUpdateReservation" value="<?php $tabInfoReservation['id_reservation'];?>" class="form-check-input">
+            <button type="submit" class="btn btn-success btn-block" id="Envoyer" >Modifier la réservation</button>
         </div>
     </form>
 </div>
