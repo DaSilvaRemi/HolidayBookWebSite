fd527b00c51a1ae6e2979bdad02a55bad2558403
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2020-12-21 18:20:25 +0100

    Ajouts suppresion, validation, refus

--- app/Controllers/GestionReservation.php
+++ app/Controllers/GestionReservation.php
@@ -4,19 +4,18 @@ use \CodeIgniter\Controller;
 use \App\Models\Session;
 
 /**
- * Classe technique permettant de visualiser en admin
+ * Classe technique permettant de visualiser les reservations en admin
  *
  * @author Mathieu
  * @author Rémi
- * @author Martin
  */
 class GestionReservation extends Controller{
-    /* 
-    fonction : Vérifie l'utilisateur est connecté et qu'il a les permissions, puis on charge la vue
-    parametre : void
-    retour : Si une erreur est détecté on retourne sur la vue et on affiche l'erreur. 
-     * Sinon on retourne sur la page de connexion pour demander à l'utilisateur de se connecté
-    */
+    /**
+     * Vérifie si l'utilisateur est connecté et que les champs du formulaire sont bien rempli
+     * 
+     * @param void
+     * @return void Si une erreur est détecté on retourne sur la vue et on affiche l'erreur. Sinon on appelle control
+     */
     public function index() {
         helper('form');
         Session::startSession();
@@ -25,6 +24,18 @@ class GestionReservation extends Controller{
         }
         
         $SiteReservationModel = new \App\Models\SiteReservationModel;
+        //Valide la réservation
+        if(!empty($this->request->getPost('idReservationValide'))){
+            $SiteReservationModel->updateisValide($this->request->getPost('idReservationValide'), "Validée");
+        }
+        //Refuse la réservation 
+        else if(!empty($this->request->getPost('idReservationRefus'))){
+            $SiteReservationModel->updateisValide($this->request->getPost('idReservationRefus'), "Refusée");
+        }
+        //Supprime la réservation
+        elseif(!empty($this->request->getPost('idReservationSuppr'))){
+            $SiteReservationModel->deleteReservation($id_reservation);
+        }
         echo view('template/header', ['iduser' => Session::getSessionData('idUser')]);
         echo view("form/gestionreservation",['tabReservation' => $SiteReservationModel->getLesReservations()]);
         echo view('template/footer');
